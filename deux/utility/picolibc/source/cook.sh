03494e9b17ec08b7e33dc25f1a98659c57ea6b2f
hustlemax <roxhustlin@gmail.com>
2025-08-20 14:50:59 +0800

    [deux] remove some from picolibc/source

--- deux/utility/picolibc/source/cook.sh
+++ deux/utility/picolibc/source/cook.sh
@@ -2,6 +2,7 @@ OUTPUT=build-arm64
 
 cmake -B$OUTPUT -GNinja \
   -DCMAKE_BUILD_TYPE=MinSizeRel \
+  -DCMAKE_INSTALL_PREFIX=../aarch64 \
   -DCMAKE_TOOLCHAIN_FILE=aarch64-none-elf.cmake \
   -Dmultilib=off \
   -Dtests=off \
